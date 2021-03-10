6205841c7bb7b790316d1a133eedee13ed4ac0c4
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2021-03-10 10:14:04 +0100

    Första

---
+++ GeneriskaKlasserÖvning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneriskaKlasserÖvning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
